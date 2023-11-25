91e659d4e746c32f716c911234359fe2f2ad75c4
karunlal <karunlalp@gmail.com>
2023-11-25 18:41:42 +0530

    added database link component

---
+++ src/DatabaseLink.jsx
@@ -0,0 +1,27 @@
+// DatabaseLink.jsx
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const DatabaseLink = () => {
+  const handleButtonClick = () => {
+    window.location.href =
+      'https://docs.google.com/spreadsheets/d/19FFwGpfQV8iaYJPQITLUhBVyoF0_UBg4uZUVGQtbgBA/edit#gid=0';
+  };
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px',
+        zIndex: '1000',
+      }}
+    >
+      <Button variant="danger" onClick={handleButtonClick}>
+        Go to Database
+      </Button>
+    </div>
+  );
+};
+
+export default DatabaseLink;
