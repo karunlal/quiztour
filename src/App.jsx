337e048e43e3a8a02913178d1eb55964e107510b
karunlal <131301918+karunlal@users.noreply.github.com>
2025-01-20 20:36:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-// App.js
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import JsonViewer from './JsonViewer';
-import TopicList from './TopicList';
-import fetchData from './DataService';
-import Quiz from './Quiz';
-//import DatabaseLink from './DatabaseLink'; // Import DatabaseLink component
-import './Styles.css';
+import React, { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import JsonViewer from './JsonViewer'
+import TopicList from './TopicList'
+import fetchData from './DataService' // Ensure this is the correct import
+import Quiz from './Quiz'
+import './Styles.css'
 
 const App = () => {
-  const [topics, setTopics] = useState([]);
+  const [topics, setTopics] = useState([])
 
   useEffect(() => {
     const fetchDataFromLink = async () => {
       try {
         const data = await fetchData(
-          'https://script.googleusercontent.com/macros/echo?user_content_key=wenjJF6TRFEaH0dKomxpTeKSQos5ocGV1hI1zkQKr4FsfW558_Zz2kFA1t34Btcrv1J8iQg0jMwqMPvCc6AH6GA1GF_sgUMHm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnAdD5CtlJ11ucnPP1D54129qsoM-e-_yvEF1HoD61auLEEADr4s_bVq-tWUrybmF_RlX0jhgdld4s2gO3cQw7F-dJoCw8JvXDg&lib=MdqEA5bqoDmq289fmHQdM4TQM98uU350M'
-        );
-        setTopics(data);
+          'https://script.google.com/macros/s/AKfycbwzYNshjkKBCuP0EMtx8ao0-j7_IUeopyiqBlz-w5pgBKGSCfiN9riaBvpLn5M1wDo/exec'
+        )
+        setTopics(data)
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error)
       }
-    };
+    }
 
-    fetchDataFromLink();
-  }, []);
+    fetchDataFromLink()
+  }, [])
 
   return (
     <div>
@@ -39,11 +37,10 @@ const App = () => {
               <TopicList topics={topics} />
             </Route>
           </Switch>
-       
         </>
       </Router>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
